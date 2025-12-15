71ef0f65a8870aaade396e12caf61575bfb4c781
Lorena Collante <profesoracollante@gmail.com>
2025-12-15 16:54:18 -0300

    backend-completo

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ const allowedOrigins = [
   "http://127.0.0.1:5173",
   "http://localhost:5174",
   "http://127.0.0.1:5174",
+  "https://nodo-cine-lore.netlify.app/",
   process.env.FRONTEND_URL,
 ];
 
